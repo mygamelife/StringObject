0154f7d162647984f8de4badd0bb76d548ad2bbf
mygamelife <mygamelife1@gmail.com>
2014-08-01 17:20:42 +0800

    Modified token.c

--- src/Token.c
+++ src/Token.c
@@ -9,6 +9,14 @@
 #include "ErrorCode.h"
 #include "CException.h"
 
+/* To extract out number token , identifier token or operator token from the expression string
+ * input :
+ *			expression is the string that we wanted to extract the token
+ * output :
+ *			return number token if number is found in string
+ *			return idendifier token if identifier is found in string
+ * 			return operator token if operator is found in string
+ */
 Token *getToken(String *expression)
 {	
 	char *tempIden; //temporary store idendifier name
@@ -17,7 +25,8 @@ Token *getToken(String *expression)
 	/*Character at first position*/
 	int charAtThisPos = expression->startindex;
 	
-	if(stringCharAtInSet(expression , charAtThisPos , numSet)) //first character is number
+	//if character start with numbers it is number token
+	if(stringCharAtInSet(expression , charAtThisPos , numSet))
 	{	
 		
 		String *removedWord = stringRemoveWordContaining (expression , numSet);	//Remove numbers in string
@@ -28,22 +37,13 @@ Token *getToken(String *expression)
 		if(stringCharAtInSet(expression , charAtThisPos , alphaSet))
 			Throw(ERR_NOT_NUMBER_TOKEN);
 		
-		// else if(stringCharAtInSet(expression , charAtThisPos , alphaNumericSet))
-		// {
-			// String *removedWord = stringRemoveWordContaining (expression , alphaNumericSet);
-			// char *idenSubString = stringSubStringInChars(removedWord , removedWord->length);
-			// tempIden = strcat(numSubString , idenSubString);
-			// Identifier *iden = identifierNew(tempIden);
-			// return (Token*)iden;
-		// }
-		
 		int integer = subStringToInteger(numSubString); //Convert substring to integer
 		Number *num = numberNew(integer); //get integer from subStringToInteger and create a new Number Token
 		
 		return (Token*)num;
 	}
 	
-	//A~Z (65~90) , (a~z) 97~122 , ('_') 95
+	//if character start with A~Z/a~z or '_' it is identifier token
 	else if(stringCharAtInSet(expression , charAtThisPos , alphaNumericSet)) 
 	{
 		String *removedWord = stringRemoveWordContaining (expression , alphaNumericSet); //Remove identifier from string
@@ -53,7 +53,7 @@ Token *getToken(String *expression)
 		return (Token*)iden;
 	}
 	
-	//first character is operator
+	//if character not start with A~Z/a~z , '_' or numbers it is operator token
 	String *removedWord = stringRemoveWordContaining (expression , opSet); //Remove operator in string	
 	char *opSubString = stringSubStringInChars(removedWord , removedWord->length); //Removed operator become substring
 	Operator *op = operatorNewByName(opSubString);
